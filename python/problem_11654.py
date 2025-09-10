e2800d55bfcf078acd6582f794f28342ef9c6cdc
swatchka <yoonsung2007@gmail.com>
2025-09-10 10:04:22 +0900

    10:04

--- python/problem_11654.py
+++ python/problem_11654.py
@@ -1,2 +1,6 @@
-plain_text = input()
-print(ord(plain_text))
+# plain_text = input()
+# print(ord(plain_text))
+
+
+def solve(a: list):
+    return sum(a) 
---
+++ python/problem_1978.py
@@ -0,0 +1,12 @@
+t = int(input())
+data = list(map(int,input().split()))
+
+count = 0
+for x in data:
+    for i in range(2,x+1):
+        if x % i == 0:
+            if x ==  i:
+                count += 1
+            
+            break
+print(count) 
